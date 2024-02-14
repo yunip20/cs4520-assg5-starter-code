2576f9ee688125d4f99c85607a89d0165e20905d
yunip20 <yunipp20@gmail.com>
2024-02-14 10:24:03 -0500

    restore

--- app/src/main/java/com/cs4520/assignment1/MainActivity.kt
+++ app/src/main/java/com/cs4520/assignment1/MainActivity.kt
@@ -1,51 +1,41 @@
 package com.cs4520.assignment1
 
+import android.app.PendingIntent.getActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.cs4520.assignment1.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
-    private fun convertDataSet(dataset: List<List<Any?>>) : List<ProductItem.Product> {
-        val result = dataset.map { item ->
-            val name = item.get(0)?.toString() ?: ""
-            val type = item.get(1)?.toString() ?: ""
-            val expiryDate = item.get(2)?.toString() ?: ""
-            val price = item.get(3)?.toString() ?: ""
-
-            ProductItem.Product(name, expiryDate, price, type)
-        }
-        return result
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val recyclerView: RecyclerView = findViewById(R.id.rvProducts)as RecyclerView
-        val adapter = ProductAdapter(convertDataSet(productsDataset))
-        recyclerView.adapter = adapter
-        val layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+        setSupportActionBar(binding.toolbar)
+        appBarConfiguration = AppBarConfiguration(
+          navController.graph
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_graph)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-    override fun onBackPressed() {
-        val navController = findNavController(R.id.nav_graph)
-        if(navController.currentDestination?.id == R.id.loginFragment){
-            finish() // close the app
-        }
-        if (!navController.popBackStack()) {
-            super.onBackPressed()
-        }
-    }
+
 }

53d24295899df9d4e0b6d87c1a96b346c4c1dcb7
PratikSymz <budhiraja.p@northeastern.edu>
2024-03-13 11:10:33 -0400

    Updated Endpoint url

--- app/src/main/java/com/cs4520/assignment5/Api.kt
+++ app/src/main/java/com/cs4520/assignment5/Api.kt
@@ -2,5 +2,5 @@ package com.cs4520.assignment4
 
 object Api {
     const val BASE_URL: String = "https://kgtttq6tg9.execute-api.us-east-2.amazonaws.com/"
-    const val ENDPOINT: String = "prod/"
+    const val ENDPOINT: String = "prod/random/"
 }
